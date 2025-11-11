0221312b03dee3841d9e19a3810105e052cf6185
Rysker <mateuszlapka2001@gmail.com>
2025-11-11 13:49:39 +0100

    First commit backend + frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/api/v1/hello/")
+      .then(response => response.json())
+      .then(data => setMessage(data.message))
+      .catch(error => console.error("Error:", error));
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>{message || "Ładowanie..."}</p>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
